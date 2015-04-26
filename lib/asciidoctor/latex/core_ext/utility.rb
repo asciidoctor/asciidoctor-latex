2106322ddbe4f6a143c1139b67bcb725f522eaf3
James Carlson <jxxcarlson@mac.com>
2015-04-26 11:07:04 +0800

    The 'apply_macros' method simplifies the chaining of a sequence of macro applications. Example: 'yoda'.apply_macros(['foo', 'bar', 'baz'])

--- lib/asciidoctor/latex/core_ext/utility.rb
+++ lib/asciidoctor/latex/core_ext/utility.rb
@@ -25,4 +25,30 @@ class String
     "\\#{macro}\{#{self}\}"
   end
 
+  #   The 'apply_macros' method simplifies the
+  #   chaining of a sequence of macro applications.
+  #   For example, we could say
+  #
+  #   'yoda'.macro('baz').macro('bar').macro('foo')
+  #
+  #         => "\\foo{\\bar{\\baz{yoda}}}"
+  #
+  #   But it is simpler to say
+  #
+  #   'yoda'.apply_macros(['foo', 'bar', 'baz'])
+  #
+  #       => "\\foo{\\bar{\\baz{yoda}}}"
+  #
+  #   Because we reverse the argument list, the
+  #   application order of the LaTeX macros
+  #   matches the order in the argument list.
+
+  def apply_macros(macro_list)
+    val = self
+    macro_list.reverse.each do |macro_name|
+      val = val.macro(macro_name)
+    end
+    val
+  end
+
 end

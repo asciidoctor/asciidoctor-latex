595a1132433a6c631a01b4119cec3b1b6effd0d4
James Carlson <jxxcarlson@mac.com>
2015-06-27 13:54:46 -0400

    Remove leading space in code for monospace: node_processor L731

--- lib/asciidoctor/latex/node_processors.rb
+++ lib/asciidoctor/latex/node_processors.rb
@@ -728,7 +728,7 @@ module Asciidoctor
           output = Asciidoctor.convert( self.text, backend: 'html')
           output
         when :monospaced
-          "\\textt\{ #{self.text}\}"
+          "\\textt\{#{self.text}\}"
         when :superscript
           "$\{\}^{#{self.text}}$"
         when :subscript

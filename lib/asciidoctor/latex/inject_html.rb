5359cd64ce234ea5323de6b0ef37aad5ba1c1ecf
James Carlson <jxxcarlson@mac.com>
2015-09-27 17:55:50 -0400

    Change http refernce to jquery at google to https

--- lib/asciidoctor/latex/inject_html.rb
+++ lib/asciidoctor/latex/inject_html.rb
@@ -23,7 +23,7 @@ module Asciidoctor::LaTeX
 </style>
 
 
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
 
 <script>
 

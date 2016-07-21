b5c9de1363de53d24d3e188d2f40d8e0cd3865e7
James Carlson <jxxcarlson@gmail.com>
2016-07-21 02:20:47 -0400

    Construct path to the data directory using __FILE__ instead of __dir__ for Opal compatbility per Mogztter's suggestion

--- lib/asciidoctor/latex.rb
+++ lib/asciidoctor/latex.rb
@@ -1,6 +1,10 @@
 module Asciidoctor
   module LaTeX
-    DATA_DIR = File.expand_path '../../data', __dir__
+    # Old -- to remove:
+    #DATA_DIR = File.expand_path '../../data', __dir__
+
+    # -- per @Mogztter's suggestion, needed for compatibility with Opal:
+    DATA_DIR = (::File.dirname ::File.dirname ::File.dirname ::File.expand_path __FILE__) + '/data'
   end
 end
 

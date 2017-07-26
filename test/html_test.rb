14c95702958bbe909a1eb82f65e1b3f1331a073e
James Carlson <jxxcarlson@mac.com>
2017-07-26 17:41:24 -0400

    Correct syntax error in test/html_test.rb:    converter_opts backend_name: 'html', 'dialect': 'latex'    Should read as:   converter_opts backend_name: 'html', dialect: 'latex'

--- test/html_test.rb
+++ test/html_test.rb
@@ -3,6 +3,6 @@ require 'test_helper'
 DocTest.examples_path = ['test/examples/html', 'test/examples/asciidoc-html']
 
 class HtmlTest < DocTest::Test
-  converter_opts backend_name: 'html', 'dialect': 'latex'
+  converter_opts backend_name: 'html', dialect: 'latex'
   generate_tests! DocTest::HTML::ExamplesSuite
 end
